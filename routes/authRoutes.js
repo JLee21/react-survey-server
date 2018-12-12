7c56e4b132cd8d91ba971cc85f2f67606048267f
Jordan <leejchris21@gmail.com>
2018-12-12 08:56:00 -0700

    working payments. add deploy steps for heroku
    Signed-off-by: Jordan <leejchris21@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,13 +8,17 @@ module.exports = app => {
     })
   );
 
-  // When Google authroizes a user that signs up or logs in, Google will redirect
-  // the user back to a URI and that is this URI
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
