e51057a1c0f696def35dee440c54e8af0651b637
Jordan <leejchris21@gmail.com>
2018-12-14 09:33:20 -0700

    mysteriously fixed google login flow
    copied over a couple of file from commit 7c56e4b and now working
    
    Signed-off-by: Jordan <leejchris21@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,4 @@
 const passport = require('passport');
-const chalk = require('chalk');
 
 module.exports = app => {
   app.get(
@@ -9,30 +8,9 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback',
-
-    (req, res, next) => {
-
-
-      passport.authenticate('google', (error, user, info) => {
-        // this will execute in any case, even if a passport strategy will find an error
-        // log everything to console
-        console.log(chalk.red(error));
-        console.log(chalk.blue('user'), user);
-        console.log(chalk.blue('info'), info);
-
-        if (error) {
-          res.status(401).send(error);
-        } else if (!user) {
-          res.status(401).send(info);
-        } else {
-          next();
-        }
-
-        res.status(401).send(info);
-      })(req, res);
-    },
-
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
     (req, res) => {
       res.redirect('/surveys');
     }
