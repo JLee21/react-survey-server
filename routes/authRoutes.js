0b92f47ee12865dd56316083a86aaf2fcbe3a541
Jordan <leejchris21@gmail.com>
2018-12-14 09:14:59 -0700

    add debugging for /auth/google/callback
    Signed-off-by: Jordan <leejchris21@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+const chalk = require('chalk');
 
 module.exports = app => {
   app.get(
@@ -8,9 +9,30 @@ module.exports = app => {
     })
   );
 
-  app.get(
-    '/auth/google/callback',
-    passport.authenticate('google'),
+  app.get('/auth/google/callback',
+
+    (req, res, next) => {
+
+
+      passport.authenticate('google', (error, user, info) => {
+        // this will execute in any case, even if a passport strategy will find an error
+        // log everything to console
+        console.log(chalk.red(error));
+        console.log(chalk.blue('user'), user);
+        console.log(chalk.blue('info'), info);
+
+        if (error) {
+          res.status(401).send(error);
+        } else if (!user) {
+          res.status(401).send(info);
+        } else {
+          next();
+        }
+
+        res.status(401).send(info);
+      })(req, res);
+    },
+
     (req, res) => {
       res.redirect('/surveys');
     }
